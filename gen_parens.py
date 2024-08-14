c8e8232f37bfdea4d4bad7c93f8cc0ae31f6d970
griffinbaker12 <96966609+griffinbaker12@users.noreply.github.com>
2024-08-13 21:43:20 -0500

    gen parens

---
+++ gen_parens.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        def backtrack(res, s="", l_rem=n, r_rem=n):
+            if l_rem == 0 and r_rem == 0:
+                res.append(s)
+                return
+
+            if l_rem > 0:
+                backtrack(res, s + "(", l_rem - 1, r_rem)
+
+            if r_rem > 0 and r_rem > l_rem:
+                backtrack(res, s + ")", l_rem, r_rem - 1)
+
+        r = []
+        backtrack(r)
+        return r
