f12556fa76efa70cfdf024b864e2edf298382303
griffinbaker12 <96966609+griffinbaker12@users.noreply.github.com>
2024-08-18 20:52:24 -0500

    fix

--- longest_consecutive_seq.py
+++ longest_consecutive_seq.py
@@ -3,9 +3,11 @@ from typing import List
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         seen = set(nums)
+        processed = set()
         longest = 0
         for n in nums:
-            if n - 1 not in seen:
+            if n not in processed and n - 1 not in seen:
+                processed.add(n)
                 current_num = n
                 while current_num + 1 in seen:
                     current_num += 1
